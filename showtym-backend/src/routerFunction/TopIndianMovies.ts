f379f4bc2955a854d113ede8b8bcdf12e0cc0b3b
omsharma29 <theomsharma100@gmail.com>
2025-08-21 11:48:55 +0530

    cacheing

--- showtym-backend/src/routerFunction/TopIndianMovies.ts
+++ showtym-backend/src/routerFunction/TopIndianMovies.ts
@@ -1,7 +1,15 @@
 import axios from "axios"
+import { redis } from "../lib/redis.js";
 
 export const TopIndianMovies = async(c: any)=>{
      try {
+
+      const cachedData = await redis.get<string>("USAmovies");
+        
+            if (cachedData !== undefined && cachedData !== null) {
+              console.log("redis is return")
+              return c.json(cachedData, 200);
+            }
   
     const fetch = await axios.get(process.env.TOP_INDIAN_MOVIES as string, {
       headers: {
@@ -9,6 +17,11 @@ export const TopIndianMovies = async(c: any)=>{
         'x-rapidapi-host': process.env.RAPIDAPI_HOST as string,
       }
     })
+
+     await redis.set("cachedkey", fetch.data, {
+          ex: 3600
+        })
+        console.log("Serving from API");
     return c.json(fetch.data, 200)
   } catch (error) {
     console.error('Error fetching weather data:', error)
--- showtym-backend/src/routerFunction/TopUSmovies.ts
+++ showtym-backend/src/routerFunction/TopUSmovies.ts
@@ -1,14 +1,24 @@
 import axios from "axios"
+import { redis } from "../lib/redis.js";
 
 export const TopUSAmovies = async(c: any)=>{
      try {
+  const cachedData = await redis.get<string>("USAmovies");
   
+      if (cachedData !== undefined && cachedData !== null) {
+        console.log("redis is return")
+        return c.json(cachedData, 200);
+      }
     const fetch = await axios.get(process.env.TOP_US_MOVIES as string, {
       headers: {
         'x-rapidapi-key': process.env.RAPIDAPI_KEY as string,
         'x-rapidapi-host': process.env.RAPIDAPI_HOST as string,
       }
     })
+     await redis.set("cachedkey", fetch.data, {
+          ex: 3600
+        })
+        console.log("Serving from API");
     return c.json(fetch.data, 200)
   } catch (error) {
     console.error('Error fetching weather data:', error)
