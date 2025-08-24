2778280671219656b922f3e7c3c61599ccb0b2ae
omsharma29 <theomsharma100@gmail.com>
2025-08-24 16:28:47 +0530

    clerk done

--- showtym-frontend/src/App.tsx
+++ showtym-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import DetailsPage from "./pages/DetailsPage"
 import SeatBook from "./pages/SeatBook"
