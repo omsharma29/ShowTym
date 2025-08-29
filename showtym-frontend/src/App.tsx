a4b4678f6ef0b0cd7818b44e68f8eee55c128286
omsharma29 <theomsharma100@gmail.com>
2025-08-29 15:03:18 +0530

    project is ready

--- showtym-frontend/src/App.tsx
+++ showtym-frontend/src/App.tsx
@@ -13,14 +13,13 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/:id">
-        <Route path="details" element={<ProtectedRoute><DetailsPage /></ProtectedRoute>} />
-        <Route path="seatBook" element={<ProtectedRoute><SeatBook /></ProtectedRoute>} />
-        <Route path="booked" element={<ProtectedRoute><Booked /></ProtectedRoute>} />
-      </Route>
       <Route path="/signup" element={<SignUp />} />
       <Route path="/history" element={<ProtectedRoute><History /></ProtectedRoute>} />
-      <Route path="/payment-status/:myOrderId" element={<PaymentStatus />} />
+      <Route path="/payment-status/:myOrderId" element={<ProtectedRoute><PaymentStatus /></ProtectedRoute>} />
+      <Route path="/:id/details" element={<ProtectedRoute><DetailsPage /></ProtectedRoute>} />
+      <Route path="/:id/seatBook" element={<ProtectedRoute><SeatBook /></ProtectedRoute>} />
+      <Route path="/:id/booked" element={<ProtectedRoute><Booked /></ProtectedRoute>} />
+      {/* This catch-all route must be the last route */}
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   )
